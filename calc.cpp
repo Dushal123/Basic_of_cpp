dd761bb5c72cac20bd7b2e8e4aa1d2c0ed0baf2e
Dushal123 <kelladushal501@gmail.com>
2025-07-12 10:02:08 +0530

    Exp_1

---
+++ calc.cpp
@@ -0,0 +1,31 @@
+/*
+Name:-Dushal Kella
+PRN:-24070123053
+Batch:-A2
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    float num1,num2;
+    cout<<"Enter the first number:";
+    cin>>num1;
+
+    cout<<"Enter the second number:";
+    cin>>num2;
+
+    cout<<"The sum of num1 and num2:"<<num1+num2<<endl;
+    cout<<"The mult of num1 and num2:"<<num1*num2<<endl;
+    cout<<"The div of num1 and num2:"<<num1/num2<<endl;
+    cout<<"The sub of num1 and num2:"<<num1-num2<<endl;
+    return 0;
+}
+/*
+Output:-
+Enter the first number:10
+Enter the second number:20
+The sum of num1 and num2:30
+The mult of num1 and num2:200
+The div of num1 and num2:0.5
+The sub of num1 and num2:-10
+*/
